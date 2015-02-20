781ba95bdb069c400d4e95587e0c3d115a79b617
Ben Kent <mr.bkent@gmail.com>
2015-02-20 23:14:23 +0000

    BBC json changes

--- episodeestimates.php
+++ episodeestimates.php
@@ -4,27 +4,67 @@
 <meta name="MobileOptimized" content="320" />
 <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scaleable=no, width=device-width" />
 </head>
+<body>
 <?php
 
- $pid = @$_POST["pid"];
+$pid = @$_POST["pid"];
+ 
+echo "<form method='post' action='http://www.bbc.co.uk/programmes/$pid' id='autosubmit' name='autosubmit' target='_blank'>";
+echo "</form>";
 
- $url = "http://www.bbc.co.uk/programmes/$pid.json";
+// none of the below works any more - the bbc has changed the json interface...
+
+/* $url = "http://www.bbc.co.uk/programmes/$pid/episodes/player.json";
  
  $json = file_get_contents($url);
+ 
+ $singleepisode = false;
+ 
+ 
+ if ($json == "")
+ {
+	$url = "http://www.bbc.co.uk/programmes/$pid.json";
+ 	$json = file_get_contents($url);
+	if ($json == "")
+	{
+		echo "Nothing found (yet)";
+		exit;
+	}
+	$singleepisode = true;
+ }
  
  $data = json_decode($json,true);
  
 //echo "<pre>";
 //print_r($data);
 //exit;
- 
-//foreach($data as $category_slice) {	
-    foreach($data as $programme) {
-        //echo "Title: " . $program['title'] . "<br />";
-		$expected_child_count = $programme['expected_child_count'];
-		echo "Estimated episodes: $expected_child_count";
+
+if (!$singleepisode)
+{ 
+	foreach($data['episodes'] as $episodes) {
+		$title = str_replace(' ', '_',$episodes['programme']['title']);
+		$position = $episodes['programme']['position'];
+		$series = str_replace(' ', '_',$episodes['programme']['programme']['title']);
+		$total = str_replace(' ', '_',$episodes['programme']['programme']['expected_child_count']);
+		$brand = str_replace(' ', '_',$episodes['programme']['programme']['programme']['title']);
+		$pid = $episodes['programme']['pid'];
+		if ($brand != "")
+			echo $brand . "_-_" . $series . "_-_" . $position . "_of_" . $total . "_" . $title . " pid: ";
+		else
+			echo $series . "_-_" . $position . "_of_" . $total . "_" . $title . " pid: ";
+		echo $pid . "<br />";
 	}
-//}
+}
+else
+{
+	foreach($data['programme'] as $programmes) {
+		$title = str_replace(' ', '_',$programmes['title']);
+		if (strlen($title) > 1)
+		    echo "Single episode! " . $pid .  "<br />" . $title;
+	}
+}*/
 
 ?>
+</body>
+<script language=JavaScript>document.autosubmit.submit();</script>
 </html>
